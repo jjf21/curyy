9cb62832d837d7f116ee2d28e6564b0f6590ea7a
jjf21 <carl.quivron@gmail.com>
2022-03-25 09:05:50 +0100

    add styling

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,19 @@
 // Entry point for the build script in your package.json
 import "@hotwired/turbo-rails"
 import "./controllers"
+
+function adjustZoom() {
+  var documentWidth = document.querySelector('#cv_container').clientWidth;
+  var documentHeight = document.querySelector('#cv_container').clientHeight;
+  // 1cm = 37.795276px;
+  // 21cm width + 1cm of margins each sides
+  // 29.7cm height + 1cm of margins each sides
+  var zoomWidth = documentWidth / (23 * 37.795276);
+  var zoomHeight = documentHeight / (31.7 * 37.795276);
+  var zoomLevel = Math.min(zoomWidth, zoomHeight);
+  // stop zooming when book fits page
+  if (zoomLevel >= 1) return;
+  document.querySelector("#cv_container").style.transform = "scale(" + zoomLevel + ")";
+}
+adjustZoom();
+window.addEventListener("resize", adjustZoom);
