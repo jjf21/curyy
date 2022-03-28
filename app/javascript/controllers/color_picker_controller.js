65bddb1fb7d1d6a3affed2792246ba5d82363a47
jjf21 <carl.quivron@gmail.com>
2022-03-28 22:38:59 +0200

    finish setting form update

--- app/javascript/controllers/color_picker_controller.js
+++ app/javascript/controllers/color_picker_controller.js
@@ -5,7 +5,7 @@ export default class extends Controller {
   static targets = [ "input" , "picker_container", "picker_icon", "palette"]
 
   connect() {
-    console.log("Connect Color Picker")
+    this.setPaletteBackground()
   }
 
   initialize() {
@@ -15,36 +15,51 @@ export default class extends Controller {
     var color = "#fff"
     if (/^#[0-9A-F]{6}$/i.test(this.inputTarget.value)) { color = this.inputTarget.value }
     document.querySelector(".page").style.background = color;
+    this.updateModel()
   }
 
   changeTextColor() {
     var color = "#000"
-    console.log(this.inputTarget.value)
     if (/^#[0-9A-F]{6}$/i.test(this.inputTarget.value)) { color = this.inputTarget.value }
-    console.log(color)
     document.querySelector(".page").style.color = color;
+    this.updateModel()
   }
 
   togglePalette() {
-    this.paletteTarget.classList.toggle('hidden')
+    this.paletteTarget.classList.toggle('h-0')
+  }
+
+  setPaletteBackground() {
+    this.picker_containerTarget.style.background = this.inputTarget.value
   }
 
   setColor(e) {
     this.inputTarget.value = e.target.dataset.color
-    this.picker_containerTarget.style.background = e.target.dataset.color
-    this.changeBackground()
+    this.setPaletteBackground()
+
+    let attr = this.inputTarget.dataset.attr;
+    if ( attr == "background" ) {
+      this.changeBackground()
+    } else if ( attr == "text" ) {
+      this.changeTextColor()
+    }
+  }
+
+  updateModel() {
+    let attr = this.inputTarget.dataset.attr;
+    let cv_id = document.querySelector("#cv_id").value
+    let formData = new FormData()
+    formData.append(`cv[${attr}]`, this.inputTarget.value);
+
+    fetch(`/cvs/${cv_id}`, {
+      body: formData,
+      method: 'PATCH',
+      dataType: 'script',
+      credentials: "include",
+      headers: {
+        "X-CSRF-Token": document.getElementsByName("csrf-token")[0].content
+      },
+    })
   }
 
 }
-
-// changeBackground() {
-//   var correctColor = false
-//   if (/^#[0-9A-F]{6}$/i.test(this.inputTarget.value)) { correctColor = true }
-//
-//   var attr = this.inputTarget.dataset.attr;
-//   if ( attr == "background" ) {
-//     var color = correctColor ? this.inputTarget.value : "#fff"
-//     document.querySelector(".page").style.background = color;
-//   }
-//
-// }
