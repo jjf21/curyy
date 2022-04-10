f616f7cfbad4346f8bce83c819880586ae1413cf
jjf21 <carl.quivron@gmail.com>
2022-04-10 13:20:28 +0200

    fix coloris

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -3,24 +3,23 @@ import "@hotwired/turbo-rails"
 import "./controllers"
 import "./components/application"
 
+import "@melloware/coloris/dist/coloris.css";
+import { coloris, init } from "@melloware/coloris";
 
 document.addEventListener("turbo:load", function() {
-
-  if (document.querySelectorAll('[data-coloris]').length > 0) {
-    Coloris({
-      alpha: false,
-      swatches: [
-        'white',
-        'black',
-        '#354856',
-        '#cfb482',
-        '#e76f51',
-        '#d62828',
-        'navy',
-        '#07b',
-        '#0096c7',
-      ]
-    })
-  }
-
-});
+  init();
+  coloris({
+    alpha: false,
+    swatches: [
+      'white',
+      'black',
+      '#354856',
+      '#cfb482',
+      '#e76f51',
+      '#d62828',
+      'navy',
+      '#07b',
+      '#0096c7',
+    ]
+  })
+})
