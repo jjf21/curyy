a67433b4715f02f054e43d1eaf2d8f9b4a1acfef
jjf21 <carl.quivron@gmail.com>
2022-04-12 12:02:09 +0200

    tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,5 +5,14 @@ module.exports = {
     './app/assets/stylesheets/**/*.css',
     './app/javascript/**/*.js',
     './app/inputs/*.rb'
-  ]
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'primary':'#8139df',
+        'secondary':'#3e0075',
+        'accent':'#ffe040',
+      }
+    },
+  },
 }
