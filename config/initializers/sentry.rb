365359e750d8809e446ec3a3383f864ffc5c8a47
jjf21 <carl.quivron@gmail.com>
2022-04-14 00:56:42 +0200

    restrict sentry

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -5,4 +5,5 @@ Sentry.init do |config|
   config.breadcrumbs_logger = [:active_support_logger, :http_logger]
   config.environment = Rails.env
   config.traces_sample_rate = 1.0
+  config.enabled_environments = ["production"]
 end
