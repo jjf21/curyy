3adf225607511f82fb7bb38ff9264b4db1ba02c6
jjf21 <carl.quivron@gmail.com>
2022-04-14 22:14:29 +0200

    add asset to precompile

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -6,7 +6,10 @@
 Rails.application.config.assets.version = "1.0"
 
 # Add additional assets to the asset load path.
-Rails.application.config.assets.paths << Rails.root.join("app", "assets", "fonts")
+Rails.application.config.assets.precompile = []
+Dir[Rails.root.join("app", "assets", "**", "*.css")].each do |file|
+  Rails.application.config.assets.precompile << file
+end
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in the app/assets
