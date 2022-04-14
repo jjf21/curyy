e9ef6104a3f0624c6a6d09149d5d42aa83528b7d
jjf21 <carl.quivron@gmail.com>
2022-04-14 10:41:23 +0200

    restric sentry

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -1,9 +1,7 @@
 # frozen_string_literal: true
 
 Sentry.init do |config|
-  config.dsn = "https://6c70829e10194bf6a833424f1428faae@o566749.ingest.sentry.io/6325538"
-  config.breadcrumbs_logger = [:active_support_logger, :http_logger]
+  config.dsn = ENV["SENTRY_DSN"] || ""
   config.environment = Rails.env
-  config.traces_sample_rate = 1.0
   config.enabled_environments = ["production"]
 end
