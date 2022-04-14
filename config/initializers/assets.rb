c50513c6fdf85ac1944881e79d896ab8d3edaf8b
jjf21 <carl.quivron@gmail.com>
2022-04-14 22:28:39 +0200

    test

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -10,8 +10,4 @@ Rails.application.config.assets.precompile = []
 Dir[Rails.root.join("app", "assets", "**", "*.css")].each do |file|
   Rails.application.config.assets.precompile << file
 end
-
-# Precompile additional assets.
-# application.js, application.css, and all non-JS/CSS in the app/assets
-# folder are already added.
-# Rails.application.config.assets.precompile += %w( admin.js admin.css )
+Rails.application.config.assets.precompile << Rails.root.join("app", "assets", "application.assets.css")
