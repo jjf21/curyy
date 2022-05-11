46c79a3f97a77c9f5eb87096804b4a02187de89b
jjf21 <carl.quivron@gmail.com>
2022-05-11 17:17:44 +0200

    fix default themes colors on seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,16 @@
 # frozen_string_literal: true
 
-Theme.find_or_create_by!(name: "hello_world", body_bg: "#cfcfcf", main_color: "#eab308", text_color: "#2b315c", font_family: "Roboto")
-Theme.find_or_create_by!(name: "theme_2", header_bg: "#f5f5f5", body_bg: "#f5f5f5", main_color: "#282c2d", text_color: "#282c2d", font_family: "Roboto")
+Theme.find_or_create_by!(
+  name: "hello_world",
+  body_bg: "#e9e6e3",
+  header_bg: "#354756",
+  main_color: "#cfb482",
+  text_color: "#354856", 
+)
+Theme.find_or_create_by!(
+  name: "theme_2",
+  body_bg: "#f5f5f5",
+  header_bg: "#f5f5f5",
+  main_color: "#282c2d",
+  text_color: "#282c2d",
+)
